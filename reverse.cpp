398fe5b4a987c0169313bf2d793fb2ad4c5c7a9b
Lino408 <157348704+Lino408@users.noreply.github.com>
2024-02-04 20:48:50 -0800

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -12,7 +12,7 @@ void reverseStr (string& str)
 int main () 
 
 {
-    string str = " Hellow and thank you world ";
+    string str = " Hello and thank you world ";
     reverseStr(str);
     cout << str;
     return 0;
